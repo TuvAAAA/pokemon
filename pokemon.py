b9e6f7dd68ca060fdc1b4d9458b6e0dfbf516178
TuvAAAA <tuva.tenghall@elev.ga.ntig.se>
2026-03-24 13:30:03 +0100

    fortsätt nästa ggn

--- pokemon.py
+++ pokemon.py
@@ -9,7 +9,8 @@ pokemons = [
             {"name": "Tackle", "power": 40},
             {"name": "Vine Whip", "power": 45}
         ],
-        "defense": 49
+        "defense": 49,
+        "catch": 45
     },
     {
         "name": "Charmander",
@@ -19,7 +20,8 @@ pokemons = [
             {"name": "Scratch", "power": 40},
             {"name": "Ember", "power": 40}
         ],
-        "defense": 43
+        "defense": 43,
+        "catch": 45
     },
 
     {
@@ -30,7 +32,8 @@ pokemons = [
             {"name": "Beast boost", "power": 139}
         ],
         "defense": 139,
-    }
+        "catch": 25
+    },
 ]
 
 pokemon = random.choice(pokemons)
@@ -41,14 +44,28 @@ print("Attacks:")
 for attack in pokemon['attacks']:
     print(f"- {attack['name']} (Power: {attack['power']})")
 print(f"Defense: {pokemon['defense']}")
+print(f"Catch: {pokemon['catch']}")
 
 attack_choice = random.choice(pokemon['attacks'])
 print(f"The pokemon used {attack_choice['name']}!")
 
-catch = input("Wanna catch it? yes is 0 and no is 1 ")
+chance = random.randint(1, 100)
 
-if catch := "0":
+catch = int(input("Wanna catch it? Yes is 0 and No is 1: "))
+
+
+if catch == 0 :
     print("Perfect! Now quick throw a pokeball!")
+    input("press enter to throw a pokeball!")
 
-else:
-    print("too bad you gonna do it anyways")
+    if chance <= pokemon['catch']:
+        input("YOU CAUGHT IT!!")
+        input("It will be put into your inventory")
+
+    else:
+        print("You missed...")
+    
+
+
+elif catch == 1:
+    print("too bad you gonna do it anyways for now")
---
+++ test.py
@@ -0,0 +1,23 @@
+import random
+
+print("Du har en 25% chans att vinna!")
+
+chans = random.randint(1, 100)
+
+if chans <= 25:
+    print("DU VANN!")
+
+elif chans >= 25:
+    print("du förlorade...")
+
+
+
+
+
+
+
+
+
+
+
+
